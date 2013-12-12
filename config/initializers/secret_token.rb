99ffa8bcc7da42ace3dbf4d3506998168892cbe8
Sunil Kumar <bg.sunilkumar1@gmail.com>
2013-12-12 18:19:34 +0530

    successfully connected

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MssqlConnect::Application.config.secret_token = '1f63ba9aa2cc30d0e7352f5096438e5a0d46a9a1e7062655587329a70a595eb9e48395253ef11655a19f5a31bdaa77540284fab746540a11eafc6e531283005f'
